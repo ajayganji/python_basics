fac2f0aa2f4ee335d83b9163a5f10751cf7f8496
ajay_ganji <ganjiajaykumar@outlook.com>
2021-05-01 12:15:02 +0530

    Added a program that tells a persons stage of life with respect to the age given

---
+++ persons_stage_of_life.py
@@ -0,0 +1,13 @@
+persons_age = 68
+if persons_age < 2:
+	print("The person is baby")
+elif (persons_age >= 2) and (persons_age < 4):
+	print("The person is a tidler")
+elif (persons_age >= 4) and (persons_age < 13):
+	print("The person is a kid")
+elif (persons_age >= 13) and (persons_age < 20):
+	print("The person is a Teenager")
+elif (persons_age >= 20) and (persons_age < 65):
+	print("The person is Adult")
+else:
+	print("The person is elder")

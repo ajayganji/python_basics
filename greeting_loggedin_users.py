3d3611977ecb363e379f6e529cec290f082216ee
ajay_ganji <ganjiajaykumar@outlook.com>
2021-05-02 22:15:18 +0530

    Made some code readability changes to file greeting logged in users

--- greeting_loggedin_users.py
+++ greeting_loggedin_users.py
@@ -1,8 +1,8 @@
 # this is basic program to greet loggid in users using list, if-else and for loops
-loggedin_users = ['user1','user2','user3','admin']
+loggedin_users=['user1','user2','user3','admin']
 if loggedin_users:
 	for user in loggedin_users:
-		if user == 'admin':
+		if user=='admin':
 			print(f"Hello {user}, Would you like to see a status report")
 		else:
 			print(f"Hello {user}, thank you for logging in again")

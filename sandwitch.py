c7f2c0c834840832aa024a775f09849718bbd530
ajay_ganji <ganjiajaykumar@outlook.com>
2021-05-09 18:36:40 +0530

    Adding program that shows how to modify lists using while loop.

---
+++ sandwitch.py
@@ -0,0 +1,18 @@
+# This program shows how to modify lists using while loop.
+sandwitch_orders = ['cheese sandwitch','chicken sandwitch','egg sandwitch','grilled chicken sandwitch','cheese sandwitch']
+finished_sandwitches = []
+#restaurant_active = True
+while sandwitch_orders:
+	for sandwitch in sandwitch_orders:
+		if sandwitch=='cheese sandwitch':
+			print("We are out of cheese sandwitch")
+			while 'cheese sandwitch' in sandwitch_orders:
+				sandwitch_orders.remove('cheese sandwitch')
+		else:
+			prepared_sandwitch = sandwitch_orders.pop()
+			finished_sandwitches.append(prepared_sandwitch)
+			print(f"I prepared your {prepared_sandwitch}.")
+print("\nThe prepared sandwitchs are: \n")
+for sandwitch in finished_sandwitches:
+	print(sandwitch.title())
+print()
